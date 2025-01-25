0fa5f88b9561b1e5c860a4e52a4b1c7e4bb8bf54
Luis Miguel Blanco <luis-miguel.blanco-alonso@kolibrigames.com>
2025-01-25 22:07:32 +0100

    Updates sounds and creates level menus

---
+++ Assets/WinLoseScript.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class WinLoseScript : MonoBehaviour
+{
+    public Transform pauseButton;
+    public Transform pauseMenu;
+    public Transform winMenu;
+    public Transform loseMenu;
+
+    public void Win()
+    {
+        if (winMenu != null)
+            winMenu.gameObject.SetActive(true);
+
+        if (pauseMenu != null)
+            winMenu.gameObject.SetActive(false);
+
+        if (pauseButton != null)
+            pauseButton.gameObject.SetActive(false);
+
+        if (loseMenu != null)
+            loseMenu.gameObject.SetActive(false);
+    }
+
+    public void Lose()
+    {
+        if (loseMenu != null)
+            loseMenu.gameObject.SetActive(true);
+
+        if (pauseMenu != null)
+            winMenu.gameObject.SetActive(false);
+
+        if (pauseButton != null)
+            pauseButton.gameObject.SetActive(false);
+
+        if (winMenu != null)
+            winMenu.gameObject.SetActive(false);
+    }
+}
