afee8ac3a42e50ef16eb1dc8c31294ad54c935ad
johnkilf <kilfeaj@tcd.ie>
2025-01-26 14:09:41 +0100

    Restrict projectiles available for level 1 and 2

--- Assets/Scripts/ProjectileButton.cs
+++ Assets/Scripts/ProjectileButton.cs
@@ -1,14 +1,29 @@
+using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class ProjectileButton : MonoBehaviour, IPointerClickHandler
 {
 
     [SerializeField] private ProjectileType projectileType;
-    
+    [SerializeField] private bool isActive = true;
+    [SerializeField] private Image projectileSprite;
+
+    private void Start()
+    {
+        if (!isActive)
+        {
+            projectileSprite.color = Color.grey;   
+        }
+    }
+
     public void OnPointerClick(PointerEventData eventData)
     {
         Debug.Log(projectileType + " clicked");
-        HudEvents.ProjectileClicked?.Invoke(projectileType);
+        if (isActive)
+        {
+            HudEvents.ProjectileClicked?.Invoke(projectileType);
+        }
     }
 }
